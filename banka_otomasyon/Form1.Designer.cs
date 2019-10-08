748a9037267d84a82651f482ed1ab705096f5171
Emirhan Sönmez <emirhansonmez96@gmail.com>
2019-10-08 18:05:33 +0300

    ikinci kayit

--- banka_otomasyon/Form1.Designer.cs
+++ banka_otomasyon/Form1.Designer.cs
@@ -80,9 +80,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(331, 130);
+            this.button1.Location = new System.Drawing.Point(331, 88);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 35);
+            this.button1.Size = new System.Drawing.Size(177, 55);
             this.button1.TabIndex = 7;
             this.button1.Text = "Hesapla";
             this.button1.UseVisualStyleBackColor = true;
